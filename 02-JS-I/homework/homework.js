13adf86833bf6e4c0d6a04a264e211daa0cda367
Franco Pinocci <pinoccifranco@gmail.com>
2023-01-25 13:49:11 -0300

    Homework 02-JS-I terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -71,7 +71,10 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+if(str1.length===str2.length) {
+  return true;
+}
+return false;
 }
 
 function menosQueNoventa(num) {
@@ -162,7 +165,17 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-}
+  if(Math.sign(numero)==1){
+    return "Es positivo";
+  }
+  else if(Math.sign(numero)==-1){
+    return "Es negativo";
+  }
+  else{
+    return false;
+  }
+  }
+
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
@@ -195,14 +208,14 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado*4;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-return base*altura;
+return (base*altura)/2;
 }
 
 
@@ -210,7 +223,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  if(euro){
+    return (euro*1.20);
+  }
 }
 
 
@@ -220,7 +235,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+if(letra=="a" ||letra=="e" ||letra=="i" ||letra=="o" ||letra=="u"){
+  return "Es vocal";
+}else if(letra.length>1) {
+  return "Dato incorrecto";
+}
+else{
+  return "Dato incorrecto";
+}
 }
 
 
