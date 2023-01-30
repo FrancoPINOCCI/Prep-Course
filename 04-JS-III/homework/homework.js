4705eb52bea4410988d702a625fc96071597a21e
Franco Pinocci <pinoccifranco@gmail.com>
2023-01-30 15:08:57 -0300

    Homework 04 terminado

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -116,6 +116,15 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var producto=1;
+  if(arguments.length<1){
+    return 0;
+  }
+
+  for(i=0;i<arguments.length; i++){
+    producto=producto*arguments[i];
+  }
+  return producto;
 
 }
 
@@ -151,6 +160,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var numero=n.toString();
+  if(numero.charAt(0)==="9"){
+    return true;
+  }
+  return false;
 
 }
 
@@ -158,7 +172,14 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
+  //Escribe tu código aquí 
+  for(i=0; i<arreglo.length; i++){
+    if(arreglo[i]===arreglo[arreglo.length - 1]){
+      return true;
+    }else{
+      return false;
+    }
+  }
 
 } 
 
@@ -174,12 +195,12 @@ function mesesDelAño(array) {
       nuevoArray.push(array[i]);
     }
   }
-  if(array.length<3){
+  if(nuevoArray.length<3){
     return "No se encontraron los meses pedidos";
-  }
-  else{
+  }else{
     return nuevoArray;
   }
+  
 }
 
 
@@ -205,6 +226,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var nuevoValor=[];
+  var suma=numero;
+  for(i=0; i<10; i++){
+    suma = suma + 2;
+    if(suma===i){
+      break;
+    }else{
+      nuevoValor.push(suma);
+    }
+  }
+  if(i<10){
+    return "Se interrumpió la ejecución";
+  }else{
+    return nuevoValor;
+  }
+
 }
 
 
@@ -215,6 +252,17 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var nuevoArray=[];
+  var suma= numero;
+  for(i=0; i<10; i++){
+    if(i===5){
+      continue;
+    }else{
+      suma=suma +2
+    }
+    nuevoArray.push(suma)
+  }
+  return nuevoArray;
 }
 
 
